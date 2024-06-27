234ddeda57d17bf206b5c4c378bde3c95dd616ce
Wells Jhones <wellsjhones@gmail.com>
2024-06-26 21:41:12 -0300

    main aplication

--- src/main/java/med/voll/api/ApiApplication.java
+++ src/main/java/med/voll/api/ApiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiApplication {
+	// main aplication
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
